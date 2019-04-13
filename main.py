33591b75fbc3fdc2bcf286d1b1a1fb85c848e74a
danilo <danilosammarco1997@gmail.com>
2019-04-13 13:12:28 +0200

    Spostati i segnali dopo la creazione di tutto il layout nella classe MainWindow. Disabilitato i pulsanti prevImage e nextImage nel caso ci siano meno di 2 immagini (quindi 0 o 1). Inserito istruzioni nella OpticDiscWindow. Inserito WARNING sui bottini 'Draw Ellipse' e 'Save annotation'. Inserita possibilità di selezionare un punto dell'annotazione dell'OpticDisc e spostarlo. Impostata provvisoriamente la finestra principale come MainWindow.

--- main.py
+++ main.py
@@ -43,24 +43,18 @@ class MainWindow(QMainWindow):
         # || GRAPHICS || - FIRST COLUMN: FOLDER -
         boxFolderLayout = QVBoxLayout()
         folder = QPushButton('Change folder', cWidget)
-        folder.clicked.connect(self.changeFolder)
         self.listWidget = QListWidget()
         self.listWidget.setMinimumSize(200,100)
-        self.listWidget.currentItemChanged.connect(self.changeImage)
         boxFolderLayout.addWidget(folder)
         boxFolderLayout.addWidget(self.listWidget)
-        self.loadImage()
         mainLayout.addLayout(boxFolderLayout)
 
         # || GRAPHICS || - SECOND COLUMN: IMAGE -
         buttonImageLayout = QVBoxLayout()
         imageLayout = QHBoxLayout()
         od = QPushButton('Optic Disc', cWidget)
-        od.clicked.connect(self.opticDisc)
         macula = QPushButton('Macula', cWidget)
-        macula.clicked.connect(self.macula)
         vessel = QPushButton('Vessel', cWidget)
-        vessel.clicked.connect(self.vessel)
         imageLayout.addWidget(od)
         imageLayout.addWidget(macula)
         imageLayout.addWidget(vessel)
@@ -77,14 +71,22 @@ class MainWindow(QMainWindow):
         buttonLayout.setSpacing(2)
         mainLayout.addLayout(buttonLayout)
         self.prevImageButton = QPushButton('Previous image', cWidget)
-        self.prevImageButton.clicked.connect(self.prevImage)
         buttonLayout.addWidget(self.prevImageButton)
         self.nextImageButton = QPushButton('Next image', cWidget)
-        self.nextImageButton.clicked.connect(self.nextImage)
         buttonLayout.addWidget(self.nextImageButton)
+        
+        #WIDGET SIGNALS
+        folder.clicked.connect(self.changeFolder)
+        od.clicked.connect(self.opticDisc)
+        macula.clicked.connect(self.macula)
+        vessel.clicked.connect(self.vessel)
+        self.prevImageButton.clicked.connect(self.prevImage)
+        self.nextImageButton.clicked.connect(self.nextImage)
+        self.listWidget.currentItemChanged.connect(self.changeImage)
 
         cWidget.setLayout(mainLayout)
         self.setCentralWidget(cWidget)
+        self.loadImage()
 
     #A new image is displayed when it is selected from the folder files
     def changeImage(self,item):
@@ -108,7 +110,10 @@ class MainWindow(QMainWindow):
     # and enable both button in the other case
     def checkPrevNextButton(self):
         row=self.listWidget.currentRow()
-        if row == 0:
+        if self.listWidget.count() < 2:
+            self.prevImageButton.setEnabled(False)
+            self.nextImageButton.setEnabled(False)
+        elif row == 0:
             self.prevImageButton.setEnabled(False)
         elif row == self.listWidget.count()-1:
             self.nextImageButton.setEnabled(False)
@@ -161,24 +166,41 @@ class MainWindow(QMainWindow):
 class OpticDiscWindow(QMainWindow):
     def __init__(self, file):
         # || GRAPHICS ||
-        QMainWindow.__init__(self)   
+        QMainWindow.__init__(self)  
+        self.checkEllipse=False 
         cWidget = QWidget(self)
-        painter=OpticDiscPaint(file)
-        self.setGeometry(10, 10, painter.pixmap.width(), painter.pixmap.height()+50) 
+        self.painter=OpticDiscPaint(file)
+        self.setGeometry(10, 10, self.painter.pixmap.width()+20, self.painter.pixmap.height()+50+100) 
         mainLayout = QVBoxLayout()
         buttonLayout=QHBoxLayout()
         drawEllipse = QPushButton('Draw ellipse', cWidget)
-        drawEllipse.clicked.connect(painter.drawEllipse)
+        drawEllipse.clicked.connect(self.drawEllipse)
         buttonLayout.addWidget(drawEllipse)
         saveAnnotation = QPushButton('Save annotation', cWidget)
-        saveAnnotation.clicked.connect(painter.saveAnnotation)
+        saveAnnotation.clicked.connect(self.saveAnnotation)
         buttonLayout.addWidget(saveAnnotation)
         mainLayout.addLayout(buttonLayout)
-        mainLayout.addWidget(painter)
+        self.instructions=QLabel("Instructions:\n1. You must select at least 5 points\n2. If you want to move an already drawn point click on it. When the point becomes red, click on the new location of the point.\n3. When you have finished selecting the points click on 'Draw ellipse'\n4. If the ellipse meets your criteria, save the annotation with the corresponding button.")
+        self.instructions.setFixedHeight(100)
+        mainLayout.addWidget(self.painter)
+        mainLayout.addWidget(self.instructions)
         cWidget.setLayout(mainLayout)
         self.setCentralWidget(cWidget)
 
+    def drawEllipse(self):
+        if self.painter.numberOfPoints() > 4:
+            self.painter.drawEllipse()
+            self.checkEllipse = True
+        else:
+            msgBox=QMessageBox.warning(None,"WARNING","You must select at least 5 points!")
+    def saveAnnotation(self):
+        if self.checkEllipse == True:
+            self.painter.saveAnnotation
+        else:
+            msgBox=QMessageBox.warning(None,"WARNING","You must draw the ellipse first!")
+
 class OpticDiscPaint(QWidget):
+
     def __init__(self, file):
         # || GRAPHICS ||
         self.drawEllipseFlag=False
@@ -186,16 +208,22 @@ class OpticDiscPaint(QWidget):
         self.pixmap = QPixmap(file) 
         self.file=file   
         self.point=[]
+        self.pointToMove=[-1,-1]
 
     def paintEvent(self, e):
         self.painter=QPainter(self)
         self.painter.drawPixmap(QRect(0, 0, self.pixmap.width(), self.pixmap.height()), self.pixmap)
-        self.pen=QPen()
-        self.pen.setWidth(3)
-        self.painter.setPen(self.pen)
+        self.penBlack=QPen(Qt.black,3)
+        self.penGreen=QPen(Qt.green,3)
+        self.painter.setPen(self.penBlack)
         if self.drawEllipseFlag == False:
             for pointToDraw in self.point:
                 self.painter.drawPoint(pointToDraw[0], pointToDraw[1])
+            if self.pointToMove != [-1,-1]:
+                self.painter.setPen(self.penGreen)
+                self.painter.drawPoint(self.pointToMove[0],self.pointToMove[1])
+                self.painter.setPen(self.penBlack)
+                self.pointToMove=[-1,-1]
         else:
             ellipse = cv2.fitEllipse(np.asarray(self.point))
             print(ellipse)
@@ -207,10 +235,18 @@ class OpticDiscPaint(QWidget):
             newPoint=[0,0]
             newPoint[0]=event.pos().x()
             newPoint[1]=event.pos().y()
-            self.point.append(newPoint)
+            for i in range(-5,5):
+                for j in range(-5,5):
+                    p=[newPoint[0]+i,newPoint[1]+j]
+                    if p in self.point:
+                        self.pointToMove=p
+            if self.pointToMove == [-1,-1]:
+                self.point.append(newPoint)
+            else:
+                self.point.remove([self.pointToMove[0],self.pointToMove[1]])
             self.update()
 
-    def drawEllipse(self, event):
+    def drawEllipse(self):
         print("DRAW")
         ellipse = cv2.fitEllipse(np.asarray(self.point))
         self.drawEllipseFlag=True
@@ -225,6 +261,9 @@ class OpticDiscPaint(QWidget):
                 textToWrite=textToWrite+"("+str(i[0])+","+str(i[1])+"),"
             #textToWrite=textToWrite+";"+ellipse
             employee_writer.writerow(textToWrite)
+
+    def numberOfPoints(self):
+        return len(self.point)
 
 class StartSession(QMainWindow):
 
@@ -327,7 +366,7 @@ class Vessel(QMainWindow):
 
 if __name__ == '__main__':
   app = QApplication(sys.argv)
-  #main = MainWindow()
-  main = StartSession()
+  main = MainWindow()
+  #main = StartSession()
   main.show()
   sys.exit(app.exec_())
